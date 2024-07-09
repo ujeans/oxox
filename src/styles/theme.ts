79aeb55d1d60c3ce35e98e9eba7b811e205c6bd7
ujeans <dbwlsxkal95@gmail.com>
2024-07-10 00:13:34 +0900

    feat(feature/#3): theme.ts에 색상, 폰트 작성하고 theme라는 하나의 객체로 export

---
+++ src/styles/theme.ts
@@ -0,0 +1,88 @@
+const colors = {
+  white: "#fff",
+  gray50: "#EBEBF5",
+  gray100: "rgba(235, 235, 245, 0.6)",
+  gray200: "#9A9DAC",
+  gray300: "rgba(154, 157, 172, 0.6)",
+  gray400: "#010618",
+  gray500: "#020618",
+  blue50: "#c6ddff",
+  blue100: "c6cdf1",
+  blue200: "#a0c2f4",
+  blue300: "#97aece",
+  blue400: "#a0add8",
+  blue500: "#536eff",
+  blue600: "#21283f",
+  red50: "#f9cccc",
+  red100: "#fe7171",
+};
+
+const typography = {
+  headers: {
+    h1: {
+      fontFamily: "Inter",
+      fontSize: "32px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+      letterSpacing: "-0.32px",
+    },
+    h2: {
+      fontFamily: "Inter",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+    },
+    h3: {
+      fontFamily: "Inter",
+      fontSize: "20px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+    },
+    h4: {
+      fontFamily: "Inter",
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+      letterSpacing: "0.28px",
+      textTransform: "uppercase",
+    },
+  },
+  paragraphs: {
+    default: {
+      fontFamily: "Inter",
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: 400,
+      lineHeight: "normal",
+    },
+    bold: {
+      fontFamily: "Inter",
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+    },
+  },
+  disclaimers: {
+    default: {
+      fontFamily: "Inter",
+      fontSize: "12px",
+      fontStyle: "normal",
+      fontWeight: 400,
+      lineHeight: "normal",
+    },
+    bold: {
+      fontFamily: "Inter",
+      fontSize: "12px",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+    },
+  },
+};
+
+export const theme = { colors, typography };
