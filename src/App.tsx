1283955d9d6b20d85995d86681765a6e66a4c077
ujeans <dbwlsxkal95@gmail.com>
2024-07-10 00:15:01 +0900

    feat(feature/#3): themeProvider로 theme.ts 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme";
 import Router from "./components/Router";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
