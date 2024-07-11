eb3cda978e1ed97f292163c2e27775cdb223c9f0
ujeans <dbwlsxkal95@gmail.com>
2024-07-12 04:30:30 +0900

    feat(feature/#10): 각 페이지 유효성 검사

--- src/containers/login/LoinForm.tsx
+++ src/containers/login/LoinForm.tsx
@@ -12,6 +12,11 @@ const LoinForm = () => {
     email: "",
     password: "",
   });
+  const [errors, setErrors] = useState<FormValues>({
+    email: "",
+    password: "",
+    nickname: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -22,15 +27,32 @@ const LoinForm = () => {
     }));
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newErrors: FormValues = { email: "", password: "", nickname: "" };
+
+    if (!value.email) {
+      newErrors.email = "이메일을 입력하세요";
+    }
+    if (!value.password) {
+      newErrors.password = "비밀번호를 입력하세요.";
+    }
+
+    setErrors(newErrors);
+  };
+
   return (
-    <FormWrapper>
+    <FormWrapper onSubmit={handleSubmit}>
       <InputWrapper>
         <Label text="이메일" />
         <Input value={value.email} onChange={handleChange} name="email" />
+        {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
       </InputWrapper>
       <InputWrapper>
         <Label text="비밀번호" />
         <Input value={value.password} onChange={handleChange} name="password" />
+        {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
       </InputWrapper>
       <Button text="이메일로 시작하기" />
     </FormWrapper>
@@ -43,4 +65,10 @@ const FormWrapper = styled.form``;
 
 const InputWrapper = styled.div`
   margin-bottom: 25px;
+`;
+
+const ErrorMessage = styled.div`
+  margin-top: 5px;
+  color: ${props => props.theme.colors.red200};
+  font-size: 0.8rem;
 `;
--- src/containers/signup/SignupFom.tsx
+++ src/containers/signup/SignupFom.tsx
@@ -14,6 +14,11 @@ const SignupFom = () => {
     nickname: "",
   });
   const [isDisabled, setIsDisabled] = useState(true);
+  const [errors, setErrors] = useState<FormValues>({
+    email: "",
+    password: "",
+    nickname: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -22,15 +27,54 @@ const SignupFom = () => {
       ...prev,
       [name]: value,
     }));
+    validateField(name, value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const validateField = (name: string, value: string) => {
+    let errorMessage = "";
+
+    switch (name) {
+      case "email":
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+          errorMessage = "유효한 이메일 주소를 입력해주세요.";
+        }
+        break;
+      case "password":
+        if (value.length < 6) {
+          errorMessage = "비밀번호는 최소 6자 이상 입력하세요.";
+        } else if (!/(?=.*[a-zA-Z])(?=.*\d)/.test(value)) {
+          errorMessage = "알파벳, 숫자를 조합한 비밀번호를 입력하세요.";
+        }
+        break;
+      case "nickname":
+        if (value.trim().length === 0) {
+          errorMessage = "닉네임은 최소 1자 이상 입력하세요.";
+        }
+        break;
+      default:
+        break;
+    }
+
+    setErrors(prev => ({
+      ...prev,
+      [name]: errorMessage,
+    }));
   };
 
   useEffect(() => {
-    const isFormValid = Object.values(value).some(filed => filed.trim() !== "");
+    const isFormValid =
+      Object.values(value).some(filed => filed.trim() !== "") &&
+      Object.values(errors).every(error => error === "");
+
     setIsDisabled(!isFormValid);
-  }, [value]);
+  }, [value, errors]);
 
   return (
-    <FormWrapper>
+    <FormWrapper onSubmit={handleSubmit}>
       <InputWrapper>
         <Label text="이메일" />
         <Input
@@ -39,6 +83,7 @@ const SignupFom = () => {
           onChange={handleChange}
           name="email"
         />
+        {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
       </InputWrapper>
       <InputWrapper>
         <Label text="비밀번호" />
@@ -48,6 +93,7 @@ const SignupFom = () => {
           onChange={handleChange}
           name="password"
         />
+        {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
       </InputWrapper>
       <InputWrapper>
         <Label text="닉네임" />
@@ -57,8 +103,9 @@ const SignupFom = () => {
           onChange={handleChange}
           name="nickname"
         />
+        {errors.nickname && <ErrorMessage>{errors.nickname}</ErrorMessage>}
       </InputWrapper>
-      <Button text="이메일로 시작하기" disabled={isDisabled} />
+      <Button text="이메일로 시작하기" disabled={isDisabled} type="submit" />
     </FormWrapper>
   );
 };
@@ -69,4 +116,10 @@ const FormWrapper = styled.form``;
 
 const InputWrapper = styled.div`
   margin-bottom: 25px;
+`;
+
+const ErrorMessage = styled.div`
+  margin-top: 5px;
+  color: ${props => props.theme.colors.red200};
+  font-size: 0.8rem;
 `;
