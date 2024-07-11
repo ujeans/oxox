f01839f56e95989d5161724ff97c80ea977157ce
ujeans <dbwlsxkal95@gmail.com>
2024-07-12 01:03:37 +0900

    feat: login 페이지 구현

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,3 +1,14 @@
+// components
+import Container from "../../components/users/Container";
+import DividerWithText from "../../components/users/DividerWithText";
+// containers
+import LoinForm from "../../containers/login/LoinForm";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <Container>
+      <DividerWithText text={"oxox 이메일로 로그인"} />
+      <LoinForm />
+    </Container>
+  );
 }
