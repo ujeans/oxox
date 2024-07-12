2dacbdca9149e73d9289f5873bcc3f85b96ae88e
ujeans <dbwlsxkal95@gmail.com>
2024-07-13 01:23:38 +0900

    feat(feature/#12): header 구현

---
+++ src/containers/layout/Header.tsx
@@ -0,0 +1,76 @@
+import styled from "@emotion/styled";
+import { IoIosArrowBack } from "react-icons/io";
+import { LuLogOut } from "react-icons/lu";
+import { useLocation, useMatch } from "react-router-dom";
+// assets
+import Eyes from "../../assets/eyes.svg";
+import Memo from "../../assets/memo.svg";
+import Tippinghand from "../../assets/tippinghand.svg";
+
+const Header = () => {
+  const location = useLocation();
+  const matchPostDetail = useMatch("/posts/:id");
+  const matchPostNew = useMatch("/posts/new");
+  const matchPostEdit = useMatch("/posts/edit/:id");
+  const matchProfile = useMatch("/profile");
+
+  const getTitle = () => {
+    if (matchPostNew) return <img src={Memo} alt="Post New" />;
+    if (matchPostEdit) return <img src={Memo} alt="Post Edit" />;
+    if (matchPostDetail) return <img src={Eyes} alt="Post Detail" />;
+    if (matchProfile) return <img src={Tippinghand} alt="Profile" />;
+    if (location.pathname === "/") return "oxox";
+    return "App";
+  };
+
+  const showBackIcon = location.pathname !== "/";
+  const showLogoutIcon = location.pathname === "/profile";
+
+  return (
+    <StyledHeader>
+      <Wrapper>
+        {showBackIcon && <BackIcon />}
+        <Title>{getTitle()}</Title>
+        {showLogoutIcon && <LogoutIcon />}
+      </Wrapper>
+    </StyledHeader>
+  );
+};
+
+export default Header;
+
+const StyledHeader = styled.header`
+  width: 490px;
+  height: 63px;
+  position: fixed;
+  top: 0;
+  margin: auto 0;
+  background-color: ${props => props.theme.colors.gray500};
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const BackIcon = styled(IoIosArrowBack)`
+  position: absolute;
+  left: 16px;
+  font-size: 24px;
+  color: ${props => props.theme.colors.gray100};
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  color: ${props => props.theme.colors.gray100};
+`;
+
+const LogoutIcon = styled(LuLogOut)`
+  position: absolute;
+  right: 16px;
+  font-size: 21px;
+  color: ${props => props.theme.colors.gray100};
+`;
