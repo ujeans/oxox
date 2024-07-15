e877af91e9ffcb51ca735438730da646db9c76f9
ujeans <dbwlsxkal95@gmail.com>
2024-07-15 16:23:44 +0900

    rename(feature/#16): 파일명에 있는 공백 제거

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -2,7 +2,7 @@
 import ContentLayout from "../../components/posts/ContentLayout";
 // containers
 import PostContent from "../../containers/postDetail/PostContent";
-import TotalComments from "../../containers/postDetail/ TotalComments";
+import TotalComments from "../../containers/postDetail/TotalComments";
 import Progressbar from "../../containers/postDetail/Progressbar";
 
 export default function PostDetail() {
