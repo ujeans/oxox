602d354b47116a61818f4fbf57a73df0778f3f67
ujeans <dbwlsxkal95@gmail.com>
2024-07-31 13:23:32 +0900

    feat(feature/#75): 프로필 이미지 없는 경우 user icon 이미지 주기

--- src/containers/comment/CommentItem.tsx
+++ src/containers/comment/CommentItem.tsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
-import {useEffect, useState} from "react";
-import {motion} from "framer-motion";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { FaCircleUser } from "react-icons/fa6";
 // assets
 import Emotion from "../../assets/emotion.svg";
 // styles
-import {buttonAnimation, iconAnimation} from "../../styles/animation";
-import {CommentDto} from "../../types/data/comment";
+import { buttonAnimation, iconAnimation } from "../../styles/animation";
+import { CommentDto } from "../../types/data/comment";
 import axiosInstance from "../../api/config";
 
 interface CommentItemProps {
@@ -42,7 +43,7 @@ const iconList = [
   },
 ];
 
-const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
+const CommentItem = ({ comment, fetchComments }: CommentItemProps) => {
   const [showEmotions, setShowEmotions] = useState(false);
   const [selectedEmotion, setSelectedEmotion] = useState<EmotionItem[]>([]);
 
@@ -51,7 +52,7 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
       .filter(([_, count]) => count > 0)
       .map(([key, count]) => {
         const icon = iconList.find(icon => icon.alt.toUpperCase() === key);
-        return icon ? {...icon, count} : null;
+        return icon ? { ...icon, count } : null;
       })
       .filter(icon => icon !== null) as EmotionItem[];
 
@@ -62,7 +63,10 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
     setShowEmotions(!showEmotions);
   };
 
-  const handleEmotionClick = async (commentId: Number, emotion: { src: string; alt: string }) => {
+  const handleEmotionClick = async (
+    commentId: Number,
+    emotion: { src: string; alt: string }
+  ) => {
     try {
       const token = localStorage.getItem("token");
 
@@ -71,9 +75,10 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
         return;
       }
 
-      const url = emotion.alt.toUpperCase() != comment.myReaction
-        ? `/reactions?commentId=${commentId}&emoji=${emotion.alt.toUpperCase()}`
-        : `/reactions?commentId=${commentId}`;
+      const url =
+        emotion.alt.toUpperCase() != comment.myReaction
+          ? `/reactions?commentId=${commentId}&emoji=${emotion.alt.toUpperCase()}`
+          : `/reactions?commentId=${commentId}`;
 
       await axiosInstance.post(url, {});
 
@@ -86,14 +91,15 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
 
   return (
     <Container key={comment.id}>
-      <Image
-        src={comment.user?.profileImage || ""}
-        alt={comment.user?.nickname || "profile image"}
-      />
+      {comment.user?.profileImage ? (
+        <Image src={comment.user.profileImage} alt={comment.user.nickname} />
+      ) : (
+        <FaCircleUser size={30} color="aliceblue" />
+      )}
       <InfoWrapper>
         <Top>
           <Nickname>{comment.user?.nickname}</Nickname>
-          {/* <Time>{comment.time}</Time> */}
+          {/* <Time>{comment.createAt}</Time> */}
         </Top>
         <Content>{comment.content}</Content>
         <EmotionContainer>
@@ -118,7 +124,7 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
                 ))}
               </IconContainer>
             ) : (
-              <Icon src={Emotion} alt="emotion"/>
+              <Icon src={Emotion} alt="emotion" />
             )}
           </EmotionBtn>
           <SelectedEmotions>
@@ -127,8 +133,10 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
                 key={emotion.alt}
                 onClick={() => handleEmotionClick(comment.id, emotion)}
               >
-                <Icon src={emotion.src} alt={emotion.alt}/>
-                <Count isSelected={comment.myReaction == emotion.alt.toUpperCase()}>
+                <Icon src={emotion.src} alt={emotion.alt} />
+                <Count
+                  isSelected={comment.myReaction == emotion.alt.toUpperCase()}
+                >
                   {emotion.count}
                 </Count>
               </SelectedEmotionWrapper>
@@ -143,91 +151,95 @@ const CommentItem = ({comment, fetchComments}: CommentItemProps) => {
 export default CommentItem;
 
 const Container = styled.div`
-    display: grid;
-    grid-template-columns: 30px 1fr;
-    grid-gap: 10px;
-    margin-bottom: 30px;
-    cursor: pointer;
+  display: grid;
+  grid-template-columns: 30px 1fr;
+  grid-gap: 10px;
+  margin-bottom: 30px;
+  cursor: pointer;
 `;
 
 const Image = styled.img`
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background-color: aliceblue;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: aliceblue;
 `;
 
 const InfoWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const Top = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    position: relative;
-    margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  margin-bottom: 5px;
 `;
 
 const Nickname = styled.div`
-    font-size: ${props => props.theme.typography.paragraphs.bold};
+  font-size: ${props => props.theme.typography.paragraphs.bold};
 `;
 
 const Time = styled.div`
-    font-size: ${props => props.theme.typography.disclaimers.default};
-    color: ${props => props.theme.colors.gray50};
+  font-size: ${props => props.theme.typography.disclaimers.default};
+  color: ${props => props.theme.colors.gray50};
 `;
 
 const Content = styled.div`
-    margin-bottom: 5px;
-    font-size: ${props => props.theme.typography.paragraphs.default};
+  margin-bottom: 5px;
+  font-size: ${props => props.theme.typography.paragraphs.default};
 `;
 
 const EmotionContainer = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const EmotionBtn = styled(motion.button)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 5px;
-    border: none;
-    border-radius: 15px;
-    background-color: #363b48;
-    cursor: pointer;
-    overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+  border: none;
+  border-radius: 15px;
+  background-color: #363b48;
+  cursor: pointer;
+  overflow: hidden;
 `;
 
 const IconContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 `;
 
 const SelectedEmotions = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const SelectedEmotionWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: 5px;
-    padding: 3px 3px;
-    border-radius: 15px;
-    background-color: #363b48;
+  display: flex;
+  align-items: center;
+  margin-right: 5px;
+  padding: 3px 3px;
+  border-radius: 15px;
+  background-color: #363b48;
 `;
 
 const Icon = styled(motion.img)`
-    width: 17px;
-    margin: 0 5px;
-    cursor: pointer;
+  width: 17px;
+  margin: 0 5px;
+  cursor: pointer;
 `;
 
 const Count = styled.span<{ isSelected?: boolean }>`
-    color: ${props => props.isSelected ? props.theme.colors.green200 : props.theme.colors.gray50};
-    font-size: ${props => props.isSelected ? props.theme.typography.disclaimers.bold : props.theme.typography.disclaimers.default};
-    margin: 0 5px 0 2px;
+  color: ${props =>
+    props.isSelected ? props.theme.colors.green200 : props.theme.colors.gray50};
+  font-size: ${props =>
+    props.isSelected
+      ? props.theme.typography.disclaimers.bold
+      : props.theme.typography.disclaimers.default};
+  margin: 0 5px 0 2px;
 `;
