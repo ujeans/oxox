2ba952a65f6cd53924aab4fc3ac2b308e668eda2
ujeans <dbwlsxkal95@gmail.com>
2024-07-31 11:10:52 +0900

    feat(feature/#70): userState를 localstorage에 저장

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,8 +1,15 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 // types
 import { UserDto } from "../types/data/user";
+
+const { persistAtom } = recoilPersist({
+  key: "localStorage",
+  storage: localStorage,
+});
 
 export const userState = atom<UserDto | null>({
   key: "userState",
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
