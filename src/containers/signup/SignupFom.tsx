2c0822837742195dd5a66811cf6f21dbdf1c709b
Kim minho <klaus9267@gmail.com>
2024-08-03 02:59:58 +0900

    feat(feature/#84): 닉네임 20글자 넘었을 때 경고 메세지 추가

--- src/containers/signup/SignupFom.tsx
+++ src/containers/signup/SignupFom.tsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 // types
-import { FormValues } from "../../types/data/user";
+import {FormValues} from "../../types/data/user";
 // components
 import Label from "../../components/common/Label";
-import { Input } from "../../components/common/Input";
+import {Input} from "../../components/common/Input";
 import Button from "../../components/common/Button";
 import ErrorMessage from "../../components/users/ErrorMessage";
 // api
@@ -26,7 +26,11 @@ const SignupFom = () => {
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
+
+    if (name === "nickname" && value.length > 21) {
+      return
+    }
 
     setValue(prev => ({
       ...prev,
@@ -50,7 +54,7 @@ const SignupFom = () => {
         },
       });
 
-      setValue({ email: "", password: "", nickname: "" });
+      setValue({email: "", password: "", nickname: ""});
 
       navigate("/login");
     } catch (error) {
@@ -77,6 +81,8 @@ const SignupFom = () => {
       case "nickname":
         if (value.trim().length === 0) {
           errorMessage = "닉네임은 최소 1자 이상 입력하세요.";
+        } else if (value.trim().length > 20) {
+          errorMessage = "닉네임은 최대 20자 이하로 입력하세요.";
         }
         break;
       default:
@@ -100,7 +106,7 @@ const SignupFom = () => {
   return (
     <FormWrapper onSubmit={handleSubmit}>
       <InputWrapper>
-        <Label text="이메일" />
+        <Label text="이메일"/>
         <Input
           placeholder="munchkin@oxox.kr"
           value={value.email}
@@ -110,7 +116,7 @@ const SignupFom = () => {
         {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
       </InputWrapper>
       <InputWrapper>
-        <Label text="비밀번호" />
+        <Label text="비밀번호"/>
         <Input
           placeholder="최소 6자 이상(알파벳, 숫자 필수)"
           value={value.password}
@@ -120,7 +126,7 @@ const SignupFom = () => {
         {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
       </InputWrapper>
       <InputWrapper>
-        <Label text="닉네임" />
+        <Label text="닉네임"/>
         <Input
           placeholder="별명을 알파벳, 한글, 숫자를 20자 이하로 입력해주세요"
           value={value.nickname || ""}
@@ -129,7 +135,7 @@ const SignupFom = () => {
         />
         {errors.nickname && <ErrorMessage>{errors.nickname}</ErrorMessage>}
       </InputWrapper>
-      <Button text="이메일로 시작하기" disabled={isDisabled} type="submit" />
+      <Button text="이메일로 시작하기" disabled={isDisabled} type="submit"/>
     </FormWrapper>
   );
 };
@@ -139,5 +145,5 @@ export default SignupFom;
 const FormWrapper = styled.form``;
 
 const InputWrapper = styled.div`
-  margin-bottom: 25px;
+    margin-bottom: 25px;
 `;
