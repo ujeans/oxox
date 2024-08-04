b146864445e525cf0eb19a8cbcaefc9c5e74607b
ujeans <dbwlsxkal95@gmail.com>
2024-08-04 19:04:08 +0900

    feat(feature/#95): 로그인, 회원가입 페이지에 추가

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,6 +1,7 @@
 // components
 import Container from "../../components/users/Container";
 import DividerWithText from "../../components/users/DividerWithText";
+import GoogleAuth from "../../components/users/GoogleAuth";
 import Navigate from "../../components/users/NavigateTo";
 // containers
 import LoginForm from "../../containers/login/LoginForm";
@@ -8,6 +9,7 @@ import LoginForm from "../../containers/login/LoginForm";
 export default function LoginPage() {
   return (
     <Container>
+      <GoogleAuth />
       <DividerWithText text={"oxox 이메일로 로그인"} />
       <LoginForm />
       <Navigate currentPage="login" />
